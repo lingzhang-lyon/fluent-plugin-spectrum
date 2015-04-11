86262d2b69eea0a516ffb65e06ae7677650f5fea
Ling Zhang <zhangling.ice@gmail.com>
2015-04-10 17:15:00 -0700

    include all varbinds in xml, daynamically set model and event_type_id, solve the "&" issue

--- lib/fluent/plugin/out_spectrum_other.rb
+++ lib/fluent/plugin/out_spectrum_other.rb
@@ -12,6 +12,9 @@ module Fluent
     config_param :pass, :string, :default => "password"
     config_param :include_raw, :string, :default => "false" #Include original object as raw
     config_param :attributes, :string, :default => "ALL" # fields to include, ALL for... well, ALL.
+    config_param :model_mh, :string, :default => "model_mh"
+    config_param :event_type_id, :string, :default => "event_type_id" 
+    config_param :debug, :bool, :default => "false" 
 
     def parseAttributes(alarmAttribute)
       key = @spectrum_access_code[alarmAttribute['@id'].to_s].to_s
@@ -31,34 +34,13 @@ module Fluent
     # This method is called before starting.
     def configure(conf)
       super 
-      # Read property file and create a hash
-      # @rename_rules = []
-      # conf_rename_rules = conf.keys.select { |k| k =~ /^rename_rule(\d+)$/ }
-      # conf_rename_rules.sort_by { |r| r.sub('rename_rule', '').to_i }.each do |r|
-      #   key_regexp, new_key = parse_rename_rule conf[r]
-
-      #   if key_regexp.nil? || new_key.nil?
-      #     raise Fluent::ConfigError, "Failed to parse: #{r} #{conf[r]}"
-      #   end
-
-      #   if @rename_rules.map { |r| r[:key_regexp] }.include? /#{key_regexp}/
-      #     raise Fluent::ConfigError, "Duplicated rules for key #{key_regexp}: #{@rename_rules}"
-      #   end
-
-      #   #@rename_rules << { key_regexp: /#{key_regexp}/, new_key: new_key }
-      #   @rename_rules << { key_regexp: key_regexp, new_key: new_key }
-      #   $log.info "Added rename key rule: #{r} #{@rename_rules.last}"
-      # end
-
-      # raise Fluent::ConfigError, "No rename rules are given" if @rename_rules.empty?
-
       # Read property file for varbinds and create a hash
       @varbinds = []
       conf_varbinds = conf.keys.select { |k| k =~ /^varbind(\d+)$/ }
       conf_varbinds.sort_by { |r| r.sub('varbind', '').to_i }.each do |r|
-        key_varbind, key_argos = parse_rename_rule conf[r]
+        key_varbind, key_source = parse_varbinds conf[r]
 
-        if key_varbind.nil? || key_argos.nil?
+        if key_varbind.nil? || key_source.nil?
           raise Fluent::ConfigError, "Failed to parse: #{r} #{conf[r]}"
         end
 
@@ -66,17 +48,19 @@ module Fluent
           raise Fluent::ConfigError, "Duplicated rules for key #{key_varbind}: #{@varbinds}"
         end
 
-        #@varbinds << { key_varbind: /#{key_varbind}/, key_argos: key_argos }
-        @varbinds << { key_varbind: key_varbind, key_argos: key_argos }
+        @varbinds << { key_varbind: key_varbind, key_source: key_source }
         $log.info "Added varbinds: #{r} #{@varbinds.last}"
       end
 
       # Setup URL Resource
-        @url = 'http://' + @endpoint.to_s + '/spectrum/restful/alarms/'
-        @events_url = 'http://' + @endpoint.to_s + '/spectrum/restful/events'
-    end
+      @events_url = 'http://' + @endpoint.to_s + '/spectrum/restful/events'
+      def resource
+        RestClient::Resource.new(@events_url, :user => @user, :password => @pass, :open_timeout => 5, :timeout => (@interval * 3))
+      end
 
-    def parse_rename_rule rule
+    end # end of def configure
+
+    def parse_varbinds rule
       if rule.match /^([^\s]+)\s+(.+)$/
         return $~.captures
       end
@@ -105,67 +89,68 @@ module Fluent
         $stderr.puts "OK!"
         ## Check if the incoming event already has an event id (alarm id) and a corresponding tag of spectrum 
         if record["event"].has_key?("event_type")  
-      		# # If the value on event_type is spectrum, then it means that it is already from spectrum and needs an update		
-          # if (record["event"]["event_type"].has_value?("alert.processed.spectrum") && record["event"].has_key?("source_event_id") )
-            # PUT alarms to update enriched fields
-
           # If the value on event_type is not spectrum, then it means that it is from 3rd party, needs to post new alert   
-          if (!record["event"]["event_type"].has_value?("alert.processed.spectrum"))
+          # if (!record["event"]["event_type"].has_value?("alert.processed.spectrum"))
+          if (record["event"]["event_type"]!="alert.processed.spectrum")
 
       			# Create an empty hash
             alertNewHash=Hash.new
 
             # Parse thro the array hash that contains name value pairs for hash mapping and add new records to a new hash
             @varbinds.each { |varbind| 
-              pp varbind[:key_varbind] varbind[:key_argos]
-              alertNewHash[varbind[:key_varbind]]=record["event"][varbind[:key_argos]]
+              pp varbind[:key_varbind]+varbind[:key_source]
+              alertNewHash[varbind[:key_varbind]]=record["event"][varbind[:key_source]]
             }
             # construct the xml
-            @xml =" 
-            <?xml version=\"1.0\" encoding=\"UTF-8\"?>
-            <rs:event-request throttlesize="10"
+            @xml ="<?xml version=\"1.0\" encoding=\"UTF-8\"?>
+            <rs:event-request throttlesize=\"10\"
               xmlns:rs=\"http://www.ca.com/spectrum/restful/schema/request\"
               xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"
               xsi:schemaLocation=\"http://www.ca.com/spectrum/restful/schema/request ../../../xsd/Request.xsd\">
               <rs:event>
                 <rs:target-models>
-                 <rs:model mh=\"0x3d4d5d\"/>
+                 <rs:model mh= \"#{model_mh}\" />
                 </rs:target-models>
              
                <!-- event ID -->
-                <rs:event-type id=\"0x057f059a\"/>
+                <rs:event-type id=\"#{event_type_id}\"/>
              
                 <!-- attributes/varbinds -->"
 
             alertNewHash.each do |attr, val| 
               if (val.nil? || val.empty?)
-                next
+                @xml += "\n <rs:varbind id=\""+ attr + "\"></rs:varbind>"
               else
-                @xml += "<rs:varbind id=\""+ attr + "\">"+ val +"</rs:varbind>"
+                # @xml += "\n <rs:varbind id=\""+ attr + "\">"+ val +"</rs:varbind>"
+                # @xml += "\n <rs:varbind id=\""+ attr + "\">"+ CGI.escape(val.to_s) +"</rs:varbind>"
+                @xml += "\n <rs:varbind id=\""+ attr + "\">"+ CGI.escapeHTML(val) +"</rs:varbind>"
               end
             end
 
-            @xml += "</rs:event>
+            @xml += "
+                    </rs:event>
                   </rs:event-request>"
-            $log.info "Rest url for post events" + @events_url
-            $log.info "xml: " +@xml
-           			
-            # begin		
-            #   responsePostAffEnt=RestClient::Resource.new(@urlrest,@user,@pass).put(@urlrest,:content_type => 'application/json')
-            # rescue Exception => e 
-            #   $log.error "Error in restful put call."
-            #   log.error e.backtrace.inspect
-            #   $log.error responsePostAffEnt
-            # end
-        
-          else 
+
+            $log.info "Rest url for post events: " + @events_url           	
+            if debug	
+              $log.info "xml: " +@xml
+            else
+              $log.info "xml: " +@xml	
+              begin		
+                # responsePostAffEnt = RestClient::Resource.new(@events_url,@user,@pass).post(@xml,:content_type => 'application/xml')
+                responsePostAffEnt = resource.post @xml,:content_type => 'application/xml',:accept => 'application/json'
+                $log.info responsePostAffEnt
+              end
+            end
+
+          else # if it is spectrum native alerts
             # For now just throw to stdout
             $log.info record["event"]
 
           end #end of if 'event_type' is 'alert.processed.spectrum'
 
         end  #end of if record have 'event_type'
-      end # end of loop for each record
+      } # end of loop for each record
     end  #end of emit
 
   end #end of class SpectrumOtherOut 
